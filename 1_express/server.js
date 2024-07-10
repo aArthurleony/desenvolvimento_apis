8f0fabe1d9b689e816514fc6552c26039bf9e0f9
= <=>
2024-07-10 08:25:58 -0300

    Conceitos sobe request

--- 1_express/server.js
+++ 1_express/server.js
@@ -5,8 +5,25 @@ const PORT = 3333;
 
 const app = express();
 
+//Aceitar JSON
+app.use(express.json())
+
 //rotas
+/** Resquest HTTP
+ * query params - ...:3333/pessoas?nome="Carlos"&idade=32
+ *  Rotas do tipo GET (filtros e buscas)
+ * route params - ...:3333/pessoas/5
+ *  Rotas do tipo GET, PUT, PATCH, DELETE(listar um elemento)
+ * body params - ...:3333/pessoas
+ *  Rotas do tipo POST (Cadastro de informações)
+*/
+
+/*Query Params */
 app.get("/users", (request, response) => {
+  // const query = request.query
+  // console.log(query)
+  const {nome, idade} = request.query
+  console.log(nome, idade)
   response.status(200).json([
     "Pessoa 1",
     "Pessoa 2",
@@ -15,6 +32,10 @@ app.get("/users", (request, response) => {
 });
 
 app.post("/users", (request, response) => {
+  // const body = request.body
+  // console.log(body)
+  const {nome, idade} = request.body
+  console.log(nome, idade)
   response.status(201).json([
     "Pessoa 1",
     "Pessoa 2",
@@ -23,7 +44,13 @@ app.post("/users", (request, response) => {
   ]);
 });
 
-app.put("/users", (request, response) => {
+app.put("/users/:id/:cpf", (request, response) => {
+  // const params = request.params.id
+  // console.log(params)
+  // const id = request.params.id
+  // const cpf = request.params.cpf
+  const {id, cpf} = request.params
+  console.log(id,cpf)
   response.status(200).json([
     "Pessoa 10",
     "Pessoa 2",
