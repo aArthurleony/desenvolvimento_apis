25ee00e08167698f2eb5c712879accaf02c6e388
Wendel <wendel824829has@gmail.com>
2024-07-22 12:38:19 -0300

    alterações feitas

--- src/Pages/TelaInicial.jsx
+++ src/Pages/TelaInicial.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import { ContainerPaginaInicial, Logo, Subtitulo, DetalheFooter } from '../Styles/TelaInicial';
-import { BotaoCadastrar, BotaoEntrar } from '../Components/Botoes';
-import logo from '../img/logo.png'; // Caminho relativo para a imagem
+import React from "react";
+import {
+  ContainerPaginaInicial,
+  Logo,
+  Subtitulo,
+  DetalheFooter,
+  DivBotao,
+} from "../Styles/TelaInicial";
+import { BotaoCadastrar, BotaoEntrar } from "../Components/Botoes";
+import logo from "../img/logo.png"; // Caminho relativo para a imagem
+import Login from "./Login";
 
 const TelaInicial = () => {
   return (
     <>
-    <ContainerPaginaInicial>
-      <Logo src={logo} alt="Logo" />
-      <Subtitulo>WR ESTOQUE</Subtitulo>
-      <BotaoCadastrar />
-      <BotaoEntrar />
-    </ContainerPaginaInicial>
-      <DetalheFooter/>
+      <ContainerPaginaInicial>
+        <Logo src={logo} alt="Logo" />
+        <Subtitulo>WR ESTOQUE</Subtitulo>
+        <DivBotao>
+          <a href={<Login/>}><BotaoCadastrar/></a>
+          <a href={<Login/>}><BotaoEntrar /></a>
+        </DivBotao>
+        <DetalheFooter />
+      </ContainerPaginaInicial>
     </>
   );
 };
--- src/Styles/TelaInicial.js
+++ src/Styles/TelaInicial.js
@@ -4,46 +4,45 @@ export const ContainerPaginaInicial = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: end;
+  gap: 30px;
   height: 80vh;
   text-align: center;
-
-  @media (max-width: 430px) and (max-height: 932px) {
-    padding: 20px;
-  }
+  background-color: white;
+  width: 430px;
+  height: 932px;
+  margin: 0 auto;
 `;
 
 export const Titulo = styled.h1`
   font-size: 50px;
   color: #333;
-  margin-bottom: 20px;
-  font-family: "Poppins", sans-serif; 
-
-  @media (max-width: 430px) and (max-height: 932px) {
-    font-size: 1.5rem;
-    color: black;
-  }
+  font-family: "Poppins", sans-serif;
+  font-size: 1.5rem;
+  color: black;
 `;
 
 export const Subtitulo = styled.h2`
   font-size: 23px;
   color: #333;
-  margin-top: -5%;
+  margin-top: -10%;
   margin-bottom: 20%;
-  font-family: 'poppins';
-  @media (max-width: 430px) and (max-height: 932px) {
-    font-size: 1.25rem;
-    color: black;
-  }
+  font-family: "poppins";
+  color: black;
 `;
 
 export const Logo = styled.img`
-  width: 250px; 
+  width: 250px;
   height: 250px;
 `;
 
+export const DivBotao = styled.div`
+  width: auto;
+  height: auto;
+  margin-bottom: 150px
+`
 export const GlobalButton = styled.button`
-  background-color: ${(props) => props.color || "#ffffff"};;
+  background-color: ${(props) => props.color || "#ffffff"};
   color: ${(props) => props.color || "#000000"};
   border-radius: ${(props) => props.borderRadius || "7px"};
   border: 2px solid #ccc;
@@ -53,35 +52,21 @@ export const GlobalButton = styled.button`
   font-size: 30px;
   cursor: pointer;
   margin: 10px;
+  padding: auto;
+  font-size: 20px;
 
   box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
-  &:hover{
-      color: white;
-      background-color: darkblue;
-    }
-
-  @media (max-width: 430px) and (max-height: 932px) {
-    padding: auto;
-    font-size: 20px;
-   
+  &:hover {
+    color: white;
+    background-color: #2b439c;
   }
 `;
-export const PageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-`;
-
-export const Content = styled.div`
-  flex: 1; 
-`;
-
 export const DetalheFooter = styled.div`
-  width: 100%; 
+  width: 432px;
   height: 50px;
-  background-color: #2B439C;
+  background-color: #2b439c;
   border-radius: 200px 200px 0 0;
-  position: absolute; 
   bottom: 0;
-  left: 0;
+
+
 `;
