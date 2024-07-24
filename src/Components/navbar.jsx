d1f26bd56c35f723200897226b7d2a0977a88b46
Wendel <wendel824829has@gmail.com>
2024-07-24 12:40:42 -0300

    pagina de dashboard alterada

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
+import { NavbarContainer, NavbarButton } from '../Styles/NavbarStyled';
 
-const Home = () => {
+const Navbar = () => {
   return (
-    <>
-      <h1>bem vindo a pagina home</h1>
-    </>
+    <NavbarContainer>
+      <NavbarButton href="#home">Home</NavbarButton>
+      <NavbarButton href="#about">About</NavbarButton>
+      <NavbarButton href="#contact">Contact</NavbarButton>
+    </NavbarContainer>
   );
 };
 
-export default Home;
+export default Navbar;
